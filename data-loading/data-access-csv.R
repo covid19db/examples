3dab4a6087cd3edf43d1e9edc17238a63b52ed55
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2021-01-25 13:34:11 +0100

    Updated example to work with surveys and world_bank tables

--- data-loading/data-access-csv.R
+++ data-loading/data-access-csv.R
@@ -6,7 +6,7 @@ library(dplyr)
 
 source = "WRD_ECDC"
 
-url_str=sprintf("https://raw.githubusercontent.com/covid19db/data/master/data-epidemiology/covid19db-epidemiology-%s.csv", source)
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-epidemiology/covid19db-epidemiology-%s.csv", source)
 df_epidemiology <- read.csv(url_str, stringsAsFactors = FALSE) 
 df_epidemiology_sel <- select(df_epidemiology, country, date, confirmed, dead)
 head(df_epidemiology_sel)
@@ -17,7 +17,7 @@ head(df_epidemiology_sel)
 source = "GOOGLE_MOBILITY"
 selected_countrycode = "GBR"
     
-url_str=sprintf("https://raw.githubusercontent.com/covid19db/data/master/data-mobility/covid19db-mobility-%s.csv", source)
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-mobility/covid19db-mobility-%s.csv", source)
 df_mobility <- read.csv(url_str, stringsAsFactors = FALSE) 
 df_mobility <- filter(df_mobility, countrycode == selected_countrycode)
 head(df_mobility)
@@ -27,7 +27,7 @@ head(df_mobility)
 
 selected_countrycode = "GBR"
 
-url_str=sprintf("https://raw.githubusercontent.com/covid19db/data/master/data-government-response/covid19db-government-response-GOVTRACK.csv")
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-government-response/covid19db-government-response-GOVTRACK.csv")
 df_govtrack <- read.csv(url_str, stringsAsFactors = FALSE)
 df_govtrack <- filter(df_govtrack, countrycode == selected_countrycode)
 head(df_govtrack)
@@ -37,17 +37,33 @@ head(df_govtrack)
 
 selected_countrycode = "GBR"
 
-url_str=sprintf("https://raw.githubusercontent.com/covid19db/data/master/data-statistics/covid19db-baseline-mortality.csv")
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-statistics/covid19db-baseline-mortality.csv")
 baseline_mortality <- read.csv(url_str, stringsAsFactors = FALSE)
 baseline_mortality <- filter(baseline_mortality, countrycode == selected_countrycode)
 head(baseline_mortality)
 
 
-# COUNTRY STATISTICS table
+# SURVEYS table 
+
+library(jsonlite)
 
 selected_countrycode = "GBR"
 
-url_str=sprintf("https://raw.githubusercontent.com/covid19db/data/master/data-statistics/covid19db-country-statistics.csv")
-df_stats <- read.csv(url_str, stringsAsFactors = FALSE)
-df_stats <- filter(df_stats, countrycode == selected_countrycode)
-head(df_stats)
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-statistics/covid19db-surveys.json")
+json_file <- jsonlite::fromJSON(url_str, simplifyDataFrame=T)
+
+df_surveys <- as.data.frame(t(do.call(rbind, json_file[1:9])))
+df_surveys$properties <- json_file$properties
+
+df_surveys <- filter(df_surveys, countrycode == selected_countrycode)
+head(df_surveys)
+
+
+# WORLD BANK table 
+
+selected_countrycode = "GBR"
+
+url_str=sprintf("https://media.githubusercontent.com/media/covid19db/data/master/data-statistics/covid19db-world_bank.csv")
+df_wb <- read.csv(url_str, stringsAsFactors = FALSE)
+df_wb <- filter(df_wb, countrycode == selected_countrycode)
+head(df_wb)
